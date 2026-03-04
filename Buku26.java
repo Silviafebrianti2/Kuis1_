1429c8d7783ee6efef00e741a1ca5d8b094d025d
Silviafebrianti2 <silviafebrianti517@gmail.com>
2026-03-04 11:04:21 +0700

    buku

---
+++ Buku26.java
@@ -0,0 +1,14 @@
+public class Buku26 {
+    String kodeBuku;
+    String judul;
+    String penulis;
+    boolean statusDipinjam;
+
+    public Buku26(String kodeBuku, String judul, String penulis) {
+        this.kodeBuku = kodeBuku;
+        this.judul = judul;
+        this.penulis = penulis;
+        this.statusDipinjam = false;
+
+    } 
+}
